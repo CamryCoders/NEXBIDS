404bd4f1f6784275870d3f6a0a8bdfdacf0a0d06
Camry <adgjls2008@gmail.com>
2026-08-11 23:52:15 +0530

    lax to none

--- Backend/src/controller/user.controller.js
+++ Backend/src/controller/user.controller.js
@@ -113,7 +113,7 @@ const loginUser=asyncHandler(async(req,res)=>{
     const options={
         httpOnly:true,
         secure:true,
-        sameSite:"lax"
+        sameSite:"none"
     }
         
         return res
