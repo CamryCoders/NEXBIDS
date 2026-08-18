1bca4935540565968dfea9007bd9ed8cbb93aa40
Camry <adgjls2008@gmail.com>
2026-08-18 17:12:59 +0530

    Polling added

--- Frontend/src/utils/socket.js
+++ Frontend/src/utils/socket.js
@@ -3,7 +3,7 @@ import {io} from 'socket.io-client'
 export const socket=io(
     "https://nexbids.onrender.com",{
         withCredentials:true,
-        transports:"polling"
+        transports:["polling"]
         
     }
 )
